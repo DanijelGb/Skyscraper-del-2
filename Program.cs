3885d2df61b4b02cef8cc3cbe3a0cd29ecfa4067
The-Dagba <danijel.grbic1@hotmail.com>
2023-09-22 11:09:23 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿var button = 101;
+var index = 1;
+var falseFloor = 0;
+
+while (index != button)
+{
+    if (index % 10 == 4)
+    {
+        falseFloor = falseFloor + 1;
+    }
+    if (index % 100 == 13)
+    {
+        falseFloor = falseFloor + 1;
+    }
+    index = index + 1;
+}
+
+if (button % 10 == 4 || button % 100 == 13)
+{
+    Console.WriteLine("kan ej omvandla till riktigt våningsnummer");
+}
+else
+{
+    Console.WriteLine(button - falseFloor);
+}
